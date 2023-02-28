ae0ed60161d49ea07c99d8283b415aaab6332c13
Marcelo Hernandez <76508651+marcelohdez@users.noreply.github.com>
2023-02-27 23:19:12 -0500

    do work

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
     
-    //Your code will return the length of the entered String
+    try (var scn = new Scanner(System.in)) { // auto close scanner
+      var word = scn.next();
+      System.out.println(word.length());
+    }
   }
 }
